4a4ff7f973e2624e9a33c5e58f21ff9f59f3c5f5
Shem Ramirez <shemramirezlopez@gmail.com>
2020-04-06 02:19:13 +0800

    Trial1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome
+                    Home
                 </div>
 
                 <div class="links">
